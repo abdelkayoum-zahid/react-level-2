fa3da20c299fbc959231148c8c8722425a97dde7
zahid abdelkayoum <abdelkayoumz@gmail.com>
2024-08-07 15:42:43 +0200

    lesson 2.2

--- react-level-2-lesson-2/src/App.jsx
+++ react-level-2-lesson-2/src/App.jsx
@@ -1,14 +1,25 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  let person = "Zahid Abdelkayoum";
-  const changeName=()=>{
-    person = "The Best ";
+  const [person , setPerson] = useState("zahid abdelkayoum");
+  const [age , setAge] = useState(28);
+  const [counter , setCounter] = useState(0);
+  const changeCounter = () => {
+    setCounter(counter+1);
   }
   return (
     <>
-      <h1>My name is {person}</h1>
-      <button onClick={changeName}>change name</button>
+      <h2>My name is {person}</h2>
+      <button
+      onClick={() => {setPerson("The best")}}
+      >change name</button>
+      <h2>My age is {age}</h2>
+      <button onClick={()=> {setAge(33)}}>change age</button>
+      <h2>the counter</h2>
+      <button onClick={changeCounter}>count is {counter}</button>
+
+
     </>
   );
 }
