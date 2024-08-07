aff9a693c0b53467d07e98327cf42213eaac111d
zahid abdelkayoum <abdelkayoumz@gmail.com>
2024-08-07 17:12:50 +0200

    lesson 2.3

--- react-level-2-lesson-2/src/App.jsx
+++ react-level-2-lesson-2/src/App.jsx
@@ -1,3 +1,4 @@
+import "./theme.css";
 import "./App.css";
 import { useState } from "react";
 
@@ -5,12 +6,19 @@ function App() {
   const [person , setPerson] = useState("zahid abdelkayoum");
   const [age , setAge] = useState(28);
   const [counter , setCounter] = useState(0);
+  const[theme, setTheme]= useState("")
   const changeCounter = () => {
     setCounter(counter+1);
   }
   return (
-    <>
-      <h2>My name is {person}</h2>
+    <div className={`App ${theme}`}>
+      <div>
+        <button onClick={() => {setTheme("light")}} style={{marginRight: "26px" }}>Light</button>
+        <button onClick={() => {setTheme("dark")}} style={{marginRight: "26px" }}>Dark</button>
+        <button onClick={() => {setTheme("gray")}} style={{marginRight: "26px" }}>Gray</button>
+        <button onClick={() => {setTheme("pink")}}>Pink</button>
+      </div>
+      <h2 style={{marginTop: "66px" }}>My name is {person}</h2>
       <button
       onClick={() => {setPerson("The best")}}
       >change name</button>
@@ -20,7 +28,7 @@ function App() {
       <button onClick={changeCounter}>count is {counter}</button>
 
 
-    </>
+    </div>
   );
 }
 
