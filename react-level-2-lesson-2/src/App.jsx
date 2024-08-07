3dcb23cf4f4cdf8382320c2dafe9e5cf9ba60496
zahid abdelkayoum <abdelkayoumz@gmail.com>
2024-08-07 12:50:59 +0200

    first commit

---
+++ react-level-2-lesson-2/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="card">
+        <button>
+          count is 0
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
