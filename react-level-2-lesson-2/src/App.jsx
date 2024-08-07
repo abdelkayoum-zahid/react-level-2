5e1ec6962a211a00635ccd896faeaa390b1816d4
zahid abdelkayoum <abdelkayoumz@gmail.com>
2024-08-07 13:14:36 +0200

    lesson 2.1

--- react-level-2-lesson-2/src/App.jsx
+++ react-level-2-lesson-2/src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 
 function App() {
+  let person = "Zahid Abdelkayoum";
+  const changeName=()=>{
+    person = "The Best ";
+  }
   return (
     <>
-      <div className="card">
-        <button>
-          count is 0
-        </button>
-      </div>
+      <h1>My name is {person}</h1>
+      <button onClick={changeName}>change name</button>
     </>
   );
 }
