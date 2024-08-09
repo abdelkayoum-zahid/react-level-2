17055abc3a03504662e700541b250877cf211a00
zahid abdelkayoum <abdelkayoumz@gmail.com>
2024-08-09 10:26:24 +0200

    lesson 4.2

--- react-level-2-lesson-2/src/App.jsx
+++ react-level-2-lesson-2/src/App.jsx
@@ -12,8 +12,23 @@ function App() {
   }
   return (
     <div className={`App ${theme}`}>
+      <div onChange={()=>{setTheme(theme == "" ? "dark" : "")}} className="btn-container" style={{marginBottom: "44px" }}>
+        <i className="fa fa-sun-o" aria-hidden="true" />
+        <label className="switch btn-color-mode-switch">
+          <input type="checkbox" name="color_mode" id="color_mode" defaultValue={1} />
+          <label
+            htmlFor="color_mode"
+            data-on="Dark"
+            data-off="Light"
+            className="btn-color-mode-switch-inner"
+          />
+        </label>
+        <i className="fa fa-moon-o" aria-hidden="true" />
+        
+      </div>
+        <button onClick={()=>{setTheme(theme == "" ? "dark" : "")}} style={{marginBottom: "44px" }}>Toggle Theme</button>
       <div>
-        <button onClick={() => {setTheme("light")}} style={{marginRight: "26px" }}>Light</button>
+        <button onClick={() => {setTheme("")}} style={{marginRight: "26px" }}>Light</button>
         <button onClick={() => {setTheme("dark")}} style={{marginRight: "26px" }}>Dark</button>
         <button onClick={() => {setTheme("gray")}} style={{marginRight: "26px" }}>Gray</button>
         <button onClick={() => {setTheme("pink")}}>Pink</button>
